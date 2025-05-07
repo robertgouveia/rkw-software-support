e075b3a68037d94515e8abae2ac648f26bbb8bea
Robert Gouveia <robertgouveia@Roberts-MacBook-Pro.local>
2025-05-07 20:30:10 +0100

    Update ver

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	currentVersion = "0.3.3"                              // The current version hardcoded in your app
+	currentVersion = "0.3.4"                              // The current version hardcoded in your app
 	repoSlug       = "robertgouveia/rkw-software-support" // GitHub repository slug
 )
 
